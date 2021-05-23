c59f4e242dd64d4ec19a4ec025ee75f1bd6569e8
jihun1352 <jihun5008@naver.com>
2021-05-23 22:29:31 +0900

    jpql

--- src/main/java/jpql/JpaMain.java
+++ src/main/java/jpql/JpaMain.java
@@ -14,22 +14,25 @@ public class JpaMain {
 
         try {
 
-            Member member = new Member();
-            member.setUsername("member1");
-            member.setAge(10);
-            em.persist(member);
+            for(int i=0; i<100; i++) {
+                Member member = new Member();
+                member.setUsername("member"+i);
+                member.setAge(i);
+                em.persist(member);
+            }
 
-            /*
-            TypedQuery<Member> query = em.createQuery("select m from Member m where m.username = :username", Member.class);
-            query.setParameter("username", "member1");
-            Member result = query.getSingleResult();
-             */
-            // 위 방식보다는 아래 방식 선호
-            Member result = em.createQuery("select m from Member m where m.username = :username", Member.class)
-                    .setParameter("username", "member1")
-                    .getSingleResult();
+            em.flush();
+            em.clear();
 
-            System.out.println("result = " + result.getUsername());
+            List<Member> result = em.createQuery("select m from Member m order by m.age desc", Member.class)
+                    .setFirstResult(1) // 1번부터 시작
+                    .setMaxResults(10) // 한페이지에 10개씩
+                    .getResultList();
+
+            System.out.println("result.size() = " + result.size());
+            for (Member member1 : result) {
+                System.out.println("member1.getUsername() = " + member1.getUsername());
+            }
 
             tx.commit();
         } catch (Exception e) {
